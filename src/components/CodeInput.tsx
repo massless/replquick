9bf10a3d65b9d888868775113d0af968ea923116
Chris Wetherell <44389+massless@users.noreply.github.com>
2025-05-10 10:54:43 -0700

    Distinct icon for Snippets

--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -204,20 +204,19 @@ export function CodeInput({
             <Sheet.Trigger asChild>
               <button>
                 <svg
-                  fill="none"
-                  strokeWidth="2"
                   xmlns="http://www.w3.org/2000/svg"
                   width="1em"
                   height="1em"
                   viewBox="0 0 24 24"
+                  fill="none"
                   stroke="currentColor"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M3 19a2 2 0 0 0 2 2h14a2 2 0 0 0 2 -2v-14a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v14z"></path>
-                  <path d="M3 9h18"></path>
-                  <path d="M9 3v18"></path>
+                  <polyline points="16 18 22 12 16 6" />
+                  <polyline points="8 6 2 12 8 18" />
+                  <line x1="12" y1="2" x2="12" y2="22" />
                 </svg>
                 Snippets
               </button>
