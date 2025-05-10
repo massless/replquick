4aebbd51ca763033e1b1217b761fbc3b1fd139ce
Chris Wetherell <44389+massless@users.noreply.github.com>
2025-05-09 23:59:59 -0700

    Make debug type size match results size

--- src/components/DebugView.tsx
+++ src/components/DebugView.tsx
@@ -1,9 +1,9 @@
-import { EvalResponse } from '../types'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { EvalResponse } from "../types";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface DebugViewProps {
-  data: EvalResponse
+  data: EvalResponse;
 }
 
 export const DebugView = ({ data }: DebugViewProps) => {
@@ -14,12 +14,13 @@ export const DebugView = ({ data }: DebugViewProps) => {
         style={tomorrow}
         customStyle={{
           margin: 0,
-          borderRadius: '4px',
-          padding: '1rem'
+          borderRadius: "4px",
+          padding: "1rem",
+          fontSize: "0.9rem",
         }}
       >
         {JSON.stringify(data, null, 2)}
       </SyntaxHighlighter>
     </div>
-  )
-}
+  );
+};
